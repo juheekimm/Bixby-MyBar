4de62166a5a1997cbe5259b35d1f91c149943d21
JunMock <kjmzzang01@gmail.com>
2019-10-18 23:01:33 +0900

    change function values

--- fireFun/functions/index.js
+++ fireFun/functions/index.js
@@ -5,6 +5,7 @@ const app = express();
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 exports.app = functions.https.onRequest(app);
+const values = require("./values");
 app.get("/single/:id", async (request, response) => {
   try {
     const id = request.params.id;
@@ -94,56 +95,9 @@ app.get("/categoryList", async (request, response) => {
   try {
     if (cateList.length == 0) {
       const userQuerySnapshot = await db.collection("CockTail").get();
-      
-      const category= ['집에서도 쉽게! 초간단 칵테일', '술자리 인싸되는 폭탄주 모음', '달콤하게 취하고 싶은 날','우와 이거 좀 신기한데?','유독 혼술이 당기는 날', '입문자 추천 칵테일','파티에서 마시기 좋은', '무알콜 칵테일 모음','외로운 날을 달달하게 만들어주는','비오는 날 마시기 좋은','취하고 싶은 날','입안가득 청량해지는 칵테일','모두가 사랑하는 칵테일'];
+    
       const x = [[],[],[],[],[],[],[],[],[],[],[],[],[]];
     
-      const y = [
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/shaker.jpg?alt=media&token=f244897a-3571-4dc2-8578-fa0924bced6b",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/shaker.jpg?alt=media&token=f244897a-3571-4dc2-8578-fa0924bced6b",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/sugar.jpg?alt=media&token=ad25f845-9a91-4571-964f-8a2d1db8b028",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/unique.jpg?alt=media&token=cc565d9d-92b5-4c1b-b096-d8e49d3466eb",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/solo.jpg?alt=media&token=16c17cff-35d3-43b1-af5e-b22cdf3059ce",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/noob.jpg?alt=media&token=47497911-9512-4b2a-8f24-f63a286e7aa1",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/party.jpg?alt=media&token=e10fc303-b3fc-44b2-8575-e3d571befd04",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/shaker.jpg?alt=media&token=f244897a-3571-4dc2-8578-fa0924bced6b",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/shaker.jpg?alt=media&token=f244897a-3571-4dc2-8578-fa0924bced6b",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/rain.jpg?alt=media&token=6c0d36c1-7f37-4625-b55e-9617a8478b63",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/blue.jpg?alt=media&token=a907d347-5252-4b14-bf11-7aeda28c6374",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/siwan.jpg?alt=media&token=4ef890ff-314c-4b60-ab33-5066301c7d54",
-      "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/shaker.jpg?alt=media&token=f244897a-3571-4dc2-8578-fa0924bced6b"
-      ];
-      const ids = [
-        '초간단',
-        '폭탄주',
-        '달콤',
-        '신기',
-        '혼술',
-        '입문자',
-        '파티에서',
-        '무알콜',
-        '외로운',
-        '비오는',
-        '취하고',
-        '청량해',
-        '사랑하는'
-      ];
-      const desc =[
-        '이렇게 간단해? 초간단 칵테일 모음',
-        '이거 하나면 술자리 인싸 가능!',
-        '맛도 분위기도 달콤하게 만들어 주는 칵테일',
-        '독특하게 생겼네? 신기하게 생겼네?,',
-        '힘들고 외로운 날. 혼술이 당긴다면?',
-        '칵테일이 낯설다면? 입문자에게 추천하는 칵테일 모음',
-        '파티에서 친구들과 먹기 좋은 칵테일',
-        '술을 못마셔도 괜찮아요! 무알콜 칵테일 모음',
-        '속상한 날 달달하게 당충전! 마시고 힘내요!',
-        '비오는 날 창가에 앉아 마시기 좋은 칵테일',
-        '취하고 싶은 날 마시기 좋은.',
-        '대신 다음날 힘들 수 있어요',
-        '입안가득 청량감을 느낄 수 있는 상큼달콤한 칵테일 모음',
-        '너무 유명해서 모두가 아는 칵테일'];
-        
       userQuerySnapshot.forEach(doc => {
         if (doc.data().majorCategory == null)  return;
         if (doc.data().majorCategory.includes('초간단')) {
@@ -216,11 +170,11 @@ app.get("/categoryList", async (request, response) => {
 
       for (var i = 0; i < 13; i++) {
         cateList.push({
-          id: ids[i],
-          name : category[i],
+          id: values.ids[i],
+          name : values.category[i],
           data: x[i],
-          img: y[i],
-          desc : desc[i]
+          img: values.img[i],
+          desc : values.desc[i]
         });
       }
     }
@@ -323,27 +277,6 @@ app.get("/searchAll", async (request, response) => {
   }
 });
 
-// const bar = {};
-// bar["shaker"] =
-//   "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/shaker.jpg?alt=media&token=f244897a-3571-4dc2-8578-fa0924bced6b";
-// bar["strainer"] =
-//   "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/strainer.jpg?alt=media&token=626639a1-31b0-455d-a401-ba267c8b520f";
-// bar["barspoon"] =
-//   "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/barspoon.jpg?alt=media&token=87ccb2be-efd3-4b08-82e2-67241ac119aa";
-// bar["muddler"] =
-//   "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/muddler.jpg?alt=media&token=df46b995-df03-435e-b2a7-7fbd6b3741ff";
-// bar["sieve"] =
-//   "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/sieve.jpg?alt=media&token=29a0b7d3-5309-420b-9b92-09522fb3c419";
-// bar["blender"] =
-//   "https://firebasestorage.googleapis.com/v0/b/myhand-bartender.appspot.com/o/blender.jpg?alt=media&token=6cefbd83-f70c-4650-9a2d-50a0e95fcf5e";
-// const me = {};
-// me["shaker"] =
-//   "쉐이커를 이용해 내용물을 흔들어 섞어주세요(숟가락으로 많이 저어주세요)";
-// me["strainer"] = "(스트레이너를 이용해) 얼음을 빼고 부어주세요";
-// me["barspoon"] = "바스푼(숟가락)을 이용해 내용물을 잘 저어주세요";
-// me["muddler"] = "머들러(숟가락)를 이용해 내용물을 충분히 으깨주세요";
-// me["sieve"] = "뜰체를 이용해 걸러주세요";
-// me["blender"] = "믹서기로 내용물을 갈아주세요";
 
 // app.get("/add", async (request, response) => {
 //   try {
--- mybartender.bartender/code/lib/utils.js
+++ mybartender.bartender/code/lib/utils.js
@@ -1,12 +1,41 @@
+// var http = require('http')
+// var console = require('console')
+// var config = require('config')
+
+// function replaceAll(str, searchStr, replaceStr) {
+//   return str.split(searchStr).join(replaceStr);
+// }
+
+// function getRecoImage(names) {
+//   let recoSplit = names.split(',');
+//   let recoName = [];
+
+//   for (let i = 0; i < recoSplit.length; i++) {
+//     let newUrl = config.get('single.url') + replaceAll(recoSplit[i], " ", "");
+//     let tempRes = http.getUrl(newUrl, options);
+
+//     if (tempRes.status == 200) {
+//       recoName.push({ url: tempRes.parsed.img });
+//     }
+//   }
+
+//   return recoName;
+// }
+
 var http = require('http')
 var console = require('console')
 var config = require('config')
+var utils = {};
 
-function replaceAll(str, searchStr, replaceStr) {
+utils.http = http
+utils.console = console
+utils.config = config
+
+utils.replaceAll = function replaceAll(str, searchStr, replaceStr) {
   return str.split(searchStr).join(replaceStr);
 }
 
-function getRecoImage(names) {
+utils.getRecoImage = function getRecoImage(names) {
   let recoSplit = names.split(',');
   let recoName = [];
 
@@ -21,3 +50,27 @@ function getRecoImage(names) {
 
   return recoName;
 }
+utils.cocktailInfo = {
+    id: undefined,
+    name: undefined,
+    category: undefined,
+    abv: undefined,
+    imageName: undefined,
+    description: undefined,
+    isbase: undefined,
+    recoName: undefined,
+    recoNamelist: undefined,
+    material: undefined,
+    subMaterial: undefined,
+    type: undefined,
+    image: undefined,
+    majorCategory: undefined,
+    subCategory: undefined
+};
+
+utils.options = {
+  format: 'json',
+  returnHeaders: true
+};
+
+module.exports = utils;
